e270fb5bed11d2bb21948215308a4171516c191d
suyeon6 <jmssk11@naver.com>
2021-11-16 20:52:55 +0900

    픽셀기반 처리 (히스토그램평활화, 명암대비스트레칭, 이진화, 두영상 덧셈/뺄셈) (256*256 크기, 흑백, .raw 파일만 지원)

---
+++ Week3/source/imagePro20190802Doc.cpp
@@ -0,0 +1,145 @@
+﻿
+// imagePro20190802Doc.cpp: CimagePro20190802Doc 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "imagePro20190802.h"
+#endif
+
+#include "imagePro20190802Doc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CimagePro20190802Doc
+
+IMPLEMENT_DYNCREATE(CimagePro20190802Doc, CDocument)
+
+BEGIN_MESSAGE_MAP(CimagePro20190802Doc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CimagePro20190802Doc 생성/소멸
+
+CimagePro20190802Doc::CimagePro20190802Doc() noexcept
+{
+	// TODO: 여기에 일회성 생성 코드를 추가합니다.
+
+}
+
+CimagePro20190802Doc::~CimagePro20190802Doc()
+{
+}
+
+BOOL CimagePro20190802Doc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 여기에 재초기화 코드를 추가합니다.
+	// SDI 문서는 이 문서를 다시 사용합니다.
+
+	return TRUE;
+}
+
+
+
+
+// CimagePro20190802Doc serialization
+
+void CimagePro20190802Doc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 여기에 저장 코드를 추가합니다.
+	}
+	else
+	{
+		// TODO: 여기에 로딩 코드를 추가합니다.
+		CFile* fp = ar.GetFile();
+		if (fp->GetLength() == 256 * 256) {
+			ar.Read(inputImage, 256 * 256);
+		}
+		else {
+			AfxMessageBox("256 * 256 크기의 파일만 사용할 수 있습니다.");
+		}
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 축소판 그림을 지원합니다.
+void CimagePro20190802Doc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 문서의 데이터를 그리려면 이 코드를 수정하십시오.
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 검색 처리기를 지원합니다.
+void CimagePro20190802Doc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 문서의 데이터에서 검색 콘텐츠를 설정합니다.
+	// 콘텐츠 부분은 ";"로 구분되어야 합니다.
+
+	// 예: strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CimagePro20190802Doc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CimagePro20190802Doc 진단
+
+#ifdef _DEBUG
+void CimagePro20190802Doc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CimagePro20190802Doc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CimagePro20190802Doc 명령
